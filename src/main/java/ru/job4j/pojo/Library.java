da33f11e086fe7b6fe1b38f64cb38e229c40594a
baltazar164 <kuzkem196@gmail.com>
2022-05-12 17:17:36 +0300

    3. Массивы и модели. [#174892 #299808]

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -7,13 +7,13 @@ public class Library {
         Book ginies = new Book("Ginies book record", 5500);
         Book code = new Book("Clean code", 700);
         Book[] books = {bible, pen, ginies, code};
-        for (Book book:books) {
+        for (Book book : books) {
             System.out.println(book.getName() + " - " + book.getCount());
         }
         Book temp = books[0];
         books[0] = books[3];
         books[3] = temp;
-        for (Book book:books) {
+        for (Book book : books) {
             if ("Clean code".equals(book.getName())) {
                 System.out.println(book.getName() + " - " + book.getCount());
             }
