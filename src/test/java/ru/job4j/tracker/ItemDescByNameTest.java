05a9a653f48123666e6baac0271594e08a26b541
baltazar164 <kuzkem196@gmail.com>
2022-05-25 15:00:55 +0300

    0. Сортировка [#10096 #304373]

--- src/test/java/ru/job4j/tracker/ItemDescByNameTest.java
+++ src/test/java/ru/job4j/tracker/ItemDescByNameTest.java
@@ -3,11 +3,10 @@ package ru.job4j.tracker;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ItemDescByNameTest {
 
@@ -21,7 +20,7 @@ public class ItemDescByNameTest {
         expected.add(new Item("Ted"));
         expected.add(new Item("Devid"));
         expected.add(new Item("Alex"));
-        Collections.sort(actual, new ItemDescByName());
+        actual.sort(new ItemDescByName());
         assertThat(expected, is(actual));
     }
 }
