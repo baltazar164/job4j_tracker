008aa8d96ce465bffeffd26ecc4ee9ffcf5ee1d9
baltazar164 <kuzkem196@gmail.com>
2022-06-04 16:52:01 +0300

    4. Сортировка номера [#221284 #306353]

--- src/main/java/ru/job4j/collection/LexSort.java
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -6,11 +6,8 @@ public class LexSort implements Comparator<String> {
 
     @Override
     public int compare(String left, String right) {
-        if (left == right) {
-            return 0;
-        }
-        left = left.substring(0, left.indexOf('.'));
-        right = right.substring(0, right.indexOf('.'));
+        left = left.split("\\.")[0];
+        right = right.split("\\.")[0];
         int leftNum = Integer.parseInt(left);
         int rightNum = Integer.parseInt(right);
         return Integer.compare(leftNum, rightNum);
