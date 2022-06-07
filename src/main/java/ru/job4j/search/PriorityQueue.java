4a6a72ac0876a2bf1acd13bfca03e399ab6bb3f1
baltazar164 <kuzkem196@gmail.com>
2022-06-07 13:28:01 +0300

    Revert "2. Local-Variable Type Inference [#70626]"
    This reverts commit 47f72360ab55cf8cdbd174bd74297754076eab1b.

--- src/main/java/ru/job4j/search/PriorityQueue.java
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -12,7 +12,7 @@ public class PriorityQueue {
      * @param task задача
      */
     public void put(Task task) {
-        var index = 0;
+        int index = 0;
         for (Task element : tasks) {
             if (task.getPriority() < element.getPriority()) {
                 break;
